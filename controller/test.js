8286d4ac1df19ffebd617244c1dc609bee9b05d3
Olivia Sabo-Rush <w.phrush@gmail.com>
2017-07-01 17:18:12 -0500

    update test to grab username then list of projects available

--- controller/test.js
+++ controller/test.js
@@ -1,27 +1,34 @@
 'use strict';
 
 var
-    config = require('../config'),
-    _ = require('lodash'),
-    util = require('util'),
-    Q = require('q'),
-    fsJson = require('fs-json')(),
-    changeCase = require('change-case'),
-    async = require('async'),
-    projects = require('./projects-copy'),
-    program = require('commander'),
-    inquirer = require('inquirer'),
-    colors = require('colors'),
-    fs = require('fs'),
-    url = require('url'),
-    exec = require('child_process').exec,
-    request = require('request'),
-    mkdirp = require('mkdirp'),
-    rimraf = require('rimraf'),
-    cancelOption = '[cancel]',
-    rootDirectory = './',
-    projectEntriesPath = 'projects/projects.json';
+  config = require('../config'),
+  _ = require('lodash'),
+  util = require('util'),
+  Q = require('q'),
+  fsJson = require('fs-json')(),
+  changeCase = require('change-case'),
+  async = require('async'),
+  github = require('./github'),
+  projects = require('./projects-copy'),
+  program = require('commander'),
+  inquirer = require('inquirer'),
+  colors = require('colors'),
+  fs = require('fs'),
+  url = require('url'),
+  exec = require('child_process').exec,
+  request = require('request'),
+  mkdirp = require('mkdirp'),
+  rimraf = require('rimraf'),
+  cancelOption = '[cancel]',
+  rootDirectory = './',
+  projectEntriesPath = 'projects/projects.json';
 
 module.exports.test = function() {
+  const username = github.grabLocalLogin();
+  console.log(username);
+  const installedProjects = projects.listProjectsOf(username);
+  console.log(installedProjects);
+  projects.selectProject(installedProjects, null, 'test');
+};
 
-}
+module.exports.test();
