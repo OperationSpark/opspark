d381d024c8035a995e531b17dc092f6b95f78882
Olivia Sabo-Rush <w.phrush@gmail.com>
2017-07-03 12:16:49 -0500

    promisify execs in test module

--- controller/child.js
+++ controller/child.js
@@ -1,27 +1,27 @@
 'use strict';
 
-var 
-    fs = require('fs'),
-    exec = require('child_process').exec,
-    spawn = require('child_process').spawn,
-    Promise = require('bluebird');
+var
+  fs = require('fs'),
+  exec = require('child_process').exec,
+  spawn = require('child_process').spawn,
+  Promise = require('bluebird');
 
 function execute(command) {
-    return new Promise(function (resolve, reject) {
-        var stderr, stdout;
-        var child = exec(command);
-        child.stdout.on('data', function(data) {
-            stdout = data;
-            console.log('stdout: ', data);
-        });
-        child.stderr.on('data', function(data) {
-            stderr = data;
-            console.log('stderr: ', data);
-        });
-        child.on('close', function(code) {
-            console.log('closing code: ' + code);
-            return resolve({stdout: stdout, stderr: stderr, code: code});
-        });
+  return new Promise(function (resolve, reject) {
+    var stderr, stdout;
+    var child = exec(command);
+    child.stdout.on('data', function(data) {
+      stdout = data;
+      console.log('stdout: ', data);
     });
+    child.stderr.on('data', function(data) {
+      stderr = data;
+      console.log('stderr: ', data);
+    });
+    child.on('close', function(code) {
+      console.log('closing code: ' + code);
+      return resolve({stdout: stdout, stderr: stderr, code: code});
+    });
+  });
 }
 module.exports.execute = execute;
--- controller/test.js
+++ controller/test.js
@@ -16,12 +16,13 @@ var
   fs = require('fs'),
   url = require('url'),
   exec = require('child_process').exec,
+  execP = require('./child').execute,
   request = require('request'),
   mkdirp = require('mkdirp'),
   rimraf = require('rimraf'),
   cancelOption = '[cancel]',
   rootDirectory = './',
-  projectsDirectory = 'projects',
+  projectsDirectory = '~/workspace/projects',
   projectEntriesPath = 'projects/projects.json';
 
 module.exports.test = function() {
@@ -46,28 +47,56 @@ function grabTests(err, project) {
   exec(cmd, function (err, stdout, stderr) {
     if (err) return console.log(`There was an error. ${err}`);
     console.log('Successfully downloaded tests!'.green);
-    runTests(project.name);
+    setEnv(project.name);
+  });
+}
+
+function setEnv(project) {
+  const enterDirectory = `cd ${projectsDirectory}/${project}/`;
+  const installDependencies = 'npm i';
+  // TODO: command breaks when running tests
+  // const cmd = `${enterDirectory} && ${installDependencies} && ${runProjectTests} && ${leaveDirectory}`;
+  const cmd = `${enterDirectory} && ${installDependencies}`;
+  execP(cmd).then(function (err, stdout, stderr) {
+    if (err) {
+      console.log(err, 'this is my error in run test');
+      return postTestCleanup(project);
+    }
+    console.log('Successfully installed dependencies!'.green);
+    runTests(project);
   });
 }
 
 function runTests(project) {
-  const enterDirectory = `cd ~/workspace/projects/${project}/`;
-  const installDependencies = 'npm i';
-  const runProjectTests = 'npm run test';
-  const leaveDirectory = 'cd ~/workspace/';
-  const cmd = `${enterDirectory} && ${installDependencies} && ${runProjectTests} && ${leaveDirectory}`;
-  exec(cmd, function (err, stdout, stderr) {
-    if (err) return console.log(`There was an error. ${err}`);
+  const enterDirectory = `cd ${projectsDirectory}/${project}/`;
+  const runProjectTests = `mocha -t ${projectsDirectory}/${project}/test/index.spec.js`;
+  // const leaveDirectory = 'cd ~/workspace/';
+  const cmd = `${enterDirectory} && ${runProjectTests}`;
+  execP(cmd).then(function (err, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (err) {
+      console.log(err, 'this is my error in run test');
+      return postTestCleanup(project);
+    }
     console.log('Successfully ran tests!'.green);
     postTestCleanup(project);
   });
 }
 
 function postTestCleanup(project) {
-  rimraf(`/workspace/projects/${project}/test`, function () {
-    console.log('Test directory removed!'.green);
-    rimraf(`/workspace/projects/${project}/node_modules`, function () {
-      console.log('Node Modules removed!'.green);
-    });
+  const removeTests = `rm -rf ${projectsDirectory}/${project}/test`;
+  const removeNodeModules = `rm -rf ${projectsDirectory}/${project}/node_modules`;
+  const cmd = `${removeTests} && ${removeNodeModules}`;
+  execP(cmd).then(function(err, stdout, stderr) {
+    if (err) console.log(err);
+    console.log('Tests and Node Modules removed!'.green);
   });
+  //
+  // rimraf(`${projectsDirectory}/${project}/test`, function () {
+  //   console.log('Test directory removed!'.green);
+  //   rimraf(`${projectsDirectory}/${project}/node_modules`, function () {
+  //     console.log('Node Modules removed!'.green);
+  //   });
+  // });
 }
