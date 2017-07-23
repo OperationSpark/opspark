2c88b4ee178b2e212bd77ab92d2f24209e15ac25
Olivia Sabo-Rush <w.phrush@gmail.com>
2017-07-22 19:57:49 -0500

    add commands for creating and deleting gists

--- controller/submit.js
+++ controller/submit.js
@@ -29,8 +29,16 @@ function checkGrade(stats) {
 module.exports.checkGrade = checkGrade;
 
 function createGist(stats) {
-  const cmd = `curl -X POST -d '{"public":true,"files":{"grade.txt":{"content":""}}}' -u ${github.grabLocalLogin()}:${github.grabLocalToken()} https://api.github.com/gists`
+  const cmd = `curl -X POST -d '{"public":true,"files":{"grade.txt":{"content":"heyheyhey"}}}' -u ${github.grabLocalLogin()}:${github.grabLocalToken()} https://api.github.com/gists`;
 
+  console.log(cmd);
+
+  exec(cmd, function(err, stdout, stderr) {
+    if (err) {
+      console.log(err);
+    }
+    console.log(stdout);
+  });
 
   // const body = {
   //   description: 'the description for this gist',
@@ -59,19 +67,28 @@ function createGist(stats) {
 module.exports.createGist = createGist;
 
 function deleteGist(stats) {
-  const options = {
-    method: 'DELETE',
-    uri: `https://api.github.com/gists/1677e545fe1616b836480ca2bc74c8b7`,
-    headers: {
-      'User-Agent': github.grabLocalLogin(),
-    },
-  };
-  rp(options)
-    .then(res => console.log(res))
-    .catch(err => console.error('upload failed:', err));
+  const id = '6c2b7ea354a19c571b8f180949f4ecae'
+
+  const cmd = `curl -X DELETE -u ${github.grabLocalLogin()}:${github.grabLocalToken()} https://api.github.com/gists/${id}`
+
+  console.log(cmd);
+
+  exec(cmd, function(err, stdout, stderr) {
+    if (err) {
+      console.log(err);
+    }
+    console.log(stdout);
+  });
+  // const options = {
+  //   method: 'DELETE',
+  //   uri: `https://api.github.com/gists/1677e545fe1616b836480ca2bc74c8b7`,
+  //   headers: {
+  //     'User-Agent': github.grabLocalLogin(),
+  //   },
+  // };
+  // rp(options)
+  //   .then(res => console.log(res))
+  //   .catch(err => console.error('upload failed:', err));
 }
 
 module.exports.deleteGist = deleteGist;
-
-
-curl -X POST -d '{"public":true,"files":{"test.txt":{"content":"String file contents"}}}' -u livrush:7583797e29ede76e50a43033d6452a9fab234d64 https://api.github.com/gists
