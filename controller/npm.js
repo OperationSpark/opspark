6a15e1889d80134d46e00187eab32f9a1d9a60ac
Olivia Sabo-Rush <w.phrush@gmail.com>
2017-07-27 15:40:36 -0500

    clean npm file

--- controller/npm.js
+++ controller/npm.js
@@ -1,30 +1,13 @@
-'use strict';
+const _ = require('lodash');
+const changeCase = require('change-case');
+const greenlight = require('./greenlight');
+const projects = require('./projects-copy');
+const inquirer = require('inquirer');
+const exec = require('child_process').exec;
+const env = require('./env');
 
-var
-  config = require('../config'),
-  _ = require('lodash'),
-  util = require('util'),
-  Q = require('q'),
-  fsJson = require('fs-json')(),
-  changeCase = require('change-case'),
-  async = require('async'),
-  github = require('./github'),
-  greenlight = require('./greenlight'),
-  projects = require('./projects-copy'),
-  program = require('commander'),
-  inquirer = require('inquirer'),
-  colors = require('colors'),
-  fs = require('fs'),
-  url = require('url'),
-  exec = require('child_process').exec,
-  request = require('request'),
-  rp = require('request-promise'),
-  mkdirp = require('mkdirp'),
-  rimraf = require('rimraf'),
-  cancelOption = '[cancel]',
-  env = require('./env'),
-  rootDirectory = `${env.home()}/workspace`,
-  projectsDirectory = `${rootDirectory}/projects`;
+const rootDirectory = `${env.home()}/workspace`;
+const projectsDirectory = `${rootDirectory}/projects`;
 
 /**
  * INSTALL
@@ -42,7 +25,7 @@ module.exports.install = function () {
         const chosenClass = _.pickBy(sessions, obj => obj.name === className);
         const session = Object.keys(chosenClass)[0];
         const projectsList = chosenClass[session].PROJECT;
-        projects.selectProject(projectsList, function (err, project) {
+        projects.selectProject(projectsList, function (error, project) {
           if (err) return console.log(err + ''.red);
           getPackageName(project);
         }, 'install');
@@ -95,7 +78,7 @@ const installPackages = function (all, project, pkg) {
     installCmd = `npm install --save ${pkg.name}`;
   }
   const cmd = `${enterDirectory} && ${installCmd}`;
-  exec(cmd, function (err, stdout, stderr) {
+  exec(cmd, function (err) {
     if (err) return console.log('error:'.red, err);
     console.log('Successfully installed'.green);
   });
