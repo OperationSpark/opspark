861a08c9b455dfcade8de78ba5bbeac68ede8b2a
Olivia Sabo-Rush <w.phrush@gmail.com>
2017-10-03 15:09:24 -0500

    update helpers

--- test/helpers/fakeHelpers.js
+++ test/helpers/fakeHelpers.js
@@ -1,27 +1,36 @@
-const dummyData = require('./dummyData');
+const fs = require('fs');
+const { dummyUser, dummyAuth, dummyGistGood } = require('./dummyData');
 
-module.exports.createClient = () => dummyData.dummyUser;
+module.exports.createClient = () => dummyUser;
 
-module.exports.getClient = () => new Promise((res, rej) => res(dummyData.dummyUser));
+module.exports.getClient = () => new Promise((res, rej) => res(dummyUser));
 
-module.exports.createGithubToken = () => `echo '${JSON.stringify(dummyData.dummyAuth)}'`;
+module.exports.createGithubToken = () => `echo '${JSON.stringify(dummyAuth)}'`;
 
 module.exports.deleteGithubToken = () => 'echo ""';
 
-module.exports.readGithubAuths = () => `echo ${dummyData.dummy}`;
+module.exports.readGithubAuths = () => `echo ${dummyUser}`;
 
 module.exports.checkGithubAuth = () => 'echo \'{ "statusCode": 200 }\'';
 
-module.exports.downloadProject = () => `echo ${dummyData.dummyUser}`;
+module.exports.downloadProject = (url, token, directory) => fs.mkdir(directory);
 
-module.exports.downloadProjectTests = () => `echo ${dummyData.dummy}`;
+module.exports.grabLocalUserID = () => `echo ${dummyUser.id}`;
 
-module.exports.downloadProjectPackage = () => `echo ${dummyData.dummy}`;
+module.exports.grabLocalLogin = () => `echo ${dummyUser.login}`;
 
-module.exports.createGistHelper = () => `echo ${dummyData.dummyGistGood}`;
+module.exports.grabLocalAuthID = () => `echo ${dummyAuth.id}`;
 
-module.exports.deleteGistHelper = () => `echo ${dummyData.dummy}`;
+module.exports.grabLocalAuthToken = () => `echo ${dummyAuth.token}`;
 
-module.exports.readGistHelper = () => `echo ${dummyData.dummy}`;
+module.exports.downloadProjectTests = () => `echo ${dummyUser}`;
+
+module.exports.downloadProjectPackage = () => `echo ${dummyUser}`;
+
+module.exports.createGistHelper = () => `echo ${dummyGistGood}`;
+
+module.exports.deleteGistHelper = () => `echo ${dummyUser}`;
+
+module.exports.readGistHelper = () => `echo ${dummyUser}`;
 
 module.exports.home = () => './test/files';
