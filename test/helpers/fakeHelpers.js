819f60262e0a0d717709fedb831458d05dcb8288
Olivia Sabo-Rush <w.phrush@gmail.com>
2017-10-03 19:21:24 -0500

    modify download functions to run scripts

--- test/helpers/fakeHelpers.js
+++ test/helpers/fakeHelpers.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const { dummyUser, dummyAuth, dummyGistGood } = require('./dummyData');
 
+module.exports.home = () => './test/files';
+
 module.exports.createClient = () => dummyUser;
 
 module.exports.getClient = () => new Promise((res, rej) => res(dummyUser));
@@ -27,21 +29,8 @@ module.exports.deleteGistHelper = () => `echo ${dummyUser}`;
 
 module.exports.readGistHelper = () => `echo ${dummyUser}`;
 
-module.exports.home = () => './test/files';
+module.exports.downloadProject = (url, token, directory) => `mkdir ${directory} ${directory}/.git ${directory}/.svn ${directory}/.master ${directory}/test`;
 
-module.exports.downloadProject = (url, token, directory) => {
-  fs.mkdirSync(directory);
-  fs.mkdirSync(`${directory}/.git`);
-  fs.mkdirSync(`${directory}/.svn`);
-  fs.mkdirSync(`${directory}/.master`);
-  fs.mkdirSync(`${directory}/test`);
-};
+module.exports.downloadProjectTests = (url, token, directory) => `mkdir ${directory}/test ${directory}/node_modules`;
 
-module.exports.downloadProjectTests = (url, token, directory) => {
-  fs.mkdirSync(`${directory}/test`);
-  fs.mkdirSync(`${directory}/node_modules`);
-};
-
-module.exports.downloadProjectPackage = (url, token, directory) => {
-  fs.writeFileSync(`${directory}/package.json`);
-};
+module.exports.downloadProjectPackage = (url, token, directory) => `touch ${directory}/package.json`;
