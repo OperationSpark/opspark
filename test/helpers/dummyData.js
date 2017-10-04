38048ce5fbf64f6cf4145d4935140d63dcdb8225
Olivia Sabo-Rush <w.phrush@gmail.com>
2017-10-04 11:44:01 -0500

    update dummy data to be json format

--- test/helpers/dummyData.js
+++ test/helpers/dummyData.js
@@ -18,18 +18,18 @@ module.exports.dummyProject = {
   _session: 'vJ3hbkCCwbei343Hz',
 };
 
-module.exports.dummyGistGood = {
-  url: 'https://api.github.com/gists/bcf378c040ea98e05d5bc946022e3c68',
-  id: 'bcf378c040ea98e05d5bc946022e3c68',
-  html_url: 'https://gist.github.com/bcf378c040ea98e05d5bc946022e3c68',
-  files: {
-    'grade.txt': {
-      filename: 'grade.txt',
-      raw_url: 'https://gist.githubusercontent.com/livrush/bcf378c040ea98e05d5bc946022e3c68/raw/3206d9a7d81f21f4749df954222a3fcad3441112/grade.txt',
-      content: '{"id":23201987,"requirementId":"v4sZfMFaanbqaWeNS","sessionId":"vJ3hbkCCwbei343Hz","type":"PROJECT","tests":16,"passes":16,"failures":0}'
+module.exports.dummyGistGood = `{
+  "url": "https://api.github.com/gists/bcf378c040ea98e05d5bc946022e3c68",
+  "id": "bcf378c040ea98e05d5bc946022e3c68",
+  "html_url": "https://gist.github.com/bcf378c040ea98e05d5bc946022e3c68",
+  "files": {
+    "grade.txt": {
+      "filename": "grade.txt",
+      "raw_url": "https://gist.githubusercontent.com/livrush/bcf378c040ea98e05d5bc946022e3c68/raw/3206d9a7d81f21f4749df954222a3fcad3441112/grade.txt",
+      "content": {"id":23201987,"requirementId":"v4sZfMFaanbqaWeNS","sessionId":"vJ3hbkCCwbei343Hz","type":"PROJECT","tests":16,"passes":16,"failures":0}
     }
-  },
-};
+  }
+}`;
 
 module.exports.dummyGistBad = {
   url: 'https://api.github.com/gists/bcf378c040ea98e05d5bc946022e3c68',
