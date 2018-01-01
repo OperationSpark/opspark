88e0c92cc7e1228c44cb2b3643e0ea46f928f21b
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-01-01 11:50:17 -0600

    Remove watchify from postinstall

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+# Source nvm for sub-shell
 . ~/.nvm/nvm.sh
 # This installs version 6 of node
 nvm install 6
 # This sets version 6 of node as the default
 nvm alias default 6
 # This installs global dependencies we will be using for projects
-npm install --global bower watchify --depth=0 --progress
+npm install --global bower --depth=0 --progress
 
 echo "Great! Have fun using OpSpark!"
