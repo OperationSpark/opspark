0b36be3fdcd83b63a6b7b098a2e9711ad9b25f56
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-01-01 14:13:29 -0600

    update install.sh to react differently depending on node version

--- install.sh
+++ install.sh
@@ -1,13 +1,29 @@
 #!/usr/bin/env bash
+# Save current npm version to variable
+NODE_V=`node -v`
+# Check if npm version is 6
+if [ ${NODE_V:1:1} != 6 ]
+then
+echo "Must update Node version. . ."
+# This installs global dependencies we will be using for projects
+npm install --global bower --depth=0 --progress
 # Source nvm for sub-shell
 . ~/.nvm/nvm.sh
+# Save current npm version to variable
+NVM_CURRENT=`nvm current`
 # This installs version 6 of node
-nvm install 6
+nvm install 6 --reinstall-packages-from=${NVM_CURRENT:1}
 # Switch to newest version 6
 nvm use 6
 # This sets version 6 of node as the default
 nvm alias default 6
-# This installs global dependencies we will be using for projects
+
+else
+
+echo "Node version good to go!"
+
 npm install --global bower --depth=0 --progress
+
+fi
 
 echo "Great! Have fun using OpSpark!"
