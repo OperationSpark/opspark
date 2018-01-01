7bb14be9a3aff1fef3b3133307978b0d96dace03
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-01-01 15:05:40 -0600

    update install.sh

--- install.sh
+++ install.sh
@@ -4,23 +4,30 @@ NODE_V=`node -v`
 echo $NODE_V
 # Check if npm version is 6
 if [ ${NODE_V:1:1} != 6 ]
+
 then
+
 echo "Must update Node version. . ."
 # This installs global dependencies we will be using for projects
 npm install --global bower --depth=0 --progress
 # Source nvm for sub-shell
 . ~/.nvm/nvm.sh
-# Save current npm version to variable
-NVM_CURRENT=`nvm current`
-echo $NVM_CURRENT
 # This installs version 6 of node
 nvm install 6 
 # Switch to newest version 6
 nvm use 6
+# Save current npm version to variable
+NVM_CURRENT=`nvm current`
+
+echo $NVM_CURRENT
 # This sets version 6 of node as the default
 nvm alias default 6
+
+echo mv /home/ubuntu/.nvm/versions/node/$NODE_V/lib/node_modules/ /home/ubuntu/.nvm/versions/node/$NVM_CURRENT/lib/node_modules/
+
+mv /home/ubuntu/.nvm/versions/node/$NODE_V/lib/node_modules/ /home/ubuntu/.nvm/versions/node/$NVM_CURRENT/lib/node_modules/
 # Reinstall packages from where they just were to v6
-nvm reinstall-packages ${NODE_V:1}
+# nvm reinstall-packages ${NODE_V:1}
 
 else
 
