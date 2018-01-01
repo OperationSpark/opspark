ef50ea383f5b77e30e9e91832c869da59f2258eb
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-01-01 11:27:47 -0600

    source nvm.sh before other postinstall scripts run

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-
+. ~/.nvm/nvm.sh
 # This installs version 6 of node
 nvm install 6
 # This sets version 6 of node as the default
 nvm alias default 6
 # This installs global dependencies we will be using for projects
-npm install --global bower watchify
+npm install --global bower watchify --depth=0 --progress
 
 echo "Great! Have fun using OpSpark!"
