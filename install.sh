7659f586f0a16136238c89815939303086770575
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-01-02 12:05:41 -0600

    update install.sh to check against full major version number, not just left-most digit, and check if it is less than 6 rather than equal to

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 # Save current npm version to variable, ex v6.11.2
 NODE_V=`node -v`
-
-# Check if npm version is 6
-if [ ${NODE_V:1:1} != 6 ]
+# Pull out the major version number
+NODE_MAJOR=`grep -oP '(?<=v).*?(?=\.)' <<< "$NODE_V"`
+# Check if npm version is less than 6
+if [ ${NODE_MAJOR} -lt 6 ]
 then
 echo -e "\n\e[31mYour current version of Node is $NODE_V, which is not compatible with opspark.\n"
 echo -e "Please \e[1m\e[34mupdate to (at least) Node version 6 using NVM \e[0m\e[31mand reinstall opspark.\n"
